61f8fc9dd9c75fbd0fdf9eeaa75d6dda1dd8d705
Fernando Binasco <fernando.binasco@gmail.com>
2020-04-10 03:57:13 -0300

    listado repositorios da api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
 
-  const [repositories, setRepositories] = useState(['projeto 1','projeto 2']);
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response =>{
+      setRepositories(response.data);
+    });
+  }, []);
 
   async function handleAddRepository() {
-    //repositories.push(`novo projeto ${Date.now()}`);
 
-    setRepositories([...repositories, `novo projeto ${Date.now()}`]);
+    //setRepositories([...repositories, `novo projeto ${Date.now()}`]);
+    //console.log(repositories);
 
-    console.log(repositories);
+    api.post('repositories',{
+      "title": `Desafio 1 GoStack ${Date.now()}`,
+      "url": "https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs",
+      "techs": ["Node,js", "React"]
+    });
     
   }
 
@@ -23,7 +33,7 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        {repositories.map(repository => <li key={repository}>{repository}</li>)}
+        {repositories.map(repository => <li key={repository.id}>{repository.title}</li>)}
         <li>
           Repositório 1
 
